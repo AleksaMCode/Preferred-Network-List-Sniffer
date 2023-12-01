0a9ed292ba6cb8023713ed298f34303fee153d9d
AleksaMCode <aleksamcode@gmail.com>
2023-12-01 17:19:50 +0000

    Format the code

--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -6,7 +6,13 @@ def sniff(interface):
     timeout = 60 * 3
     start_time = time.time()
 
-    airodump = subprocess.Popen(['airodump-ng', interface], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, bufsize=1)
+    airodump = subprocess.Popen(
+        ["airodump-ng", interface],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        universal_newlines=True,
+        bufsize=1,
+    )
 
     while time.time() < start_time + timeout:
         print(airodump.stdout.readline())

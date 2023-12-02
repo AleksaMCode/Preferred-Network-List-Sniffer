1b0a0c1db994013252230a4449de8531a56d0313
AleksaMCode <aleksamcode@gmail.com>
2023-12-02 21:44:34 +0100

    Implement traffic file parsing

---
+++ sniffer/parser.py
@@ -0,0 +1,23 @@
+from typing import Dict
+
+import pyshark
+from datetime import datetime
+
+from sniffer import TRAFFIC_FILE
+
+
+def parse_traffic_file() -> Dict[str, str]:
+    """
+    Parse *.cap file in order to get SSIDs from device's Preferred Network List.
+    :return: Dictionary with SSID-timestamp pairs.
+    """
+    ssids = {}
+    # airodump-ng names output with an automatic numbers, where the first file contains '-01'.
+    capture = pyshark.FileCapture(f"{TRAFFIC_FILE}-01.cap")
+    # TODO: Optimize by implementing skipping already read packages.
+    for package in capture:
+        # Filter only Probe Request and ignore Probe Requests with wildcard in the SSID field.
+        if "Type/Subtype: Probe Request" in str(package.wlan) and "Wildcard" not in package['wlan.mgt'].wlan_tag:
+            ssid = package['wlan.mgt'].wlan_ssid
+            ssids[ssid] = datetime.utcfromtimestamp(float(package.sniff_timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+    return ssids

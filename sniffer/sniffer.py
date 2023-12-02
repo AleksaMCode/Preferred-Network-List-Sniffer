bf6c99cd7d23f3b998296d084eab7de4a8ffc122
AleksaMCode <aleksamcode@gmail.com>
2023-12-02 20:39:41 +0100

    Implement file cleanup

--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -1,11 +1,27 @@
+import os
+import re
 import subprocess
+import sys
+
+TRAFFIC_FILE = "traffic"
+
+
+def clear_old_traffic():
+    """
+    Removes old traffic files
+    """
+    for file in os.listdir("."):
+        if re.match(f"{TRAFFIC_FILE}.*\.cap", file):
+            os.remove(file)
 
 
 def sniff(interface):
     subprocess.run(
-        ["airodump-ng", "-w", "traffic", "--output-format", "cap", interface],
+        ["airodump-ng", "-w", TRAFFIC_FILE, "--output-format", "cap", interface],
         capture_output=True,
     )
 
 
-sniff("wlan1mon")
+clear_old_traffic()
+# TODO: Run sniffing on new thread.
+sniff(f"{sys.argv[1]}mon")

759c07d1879bc34f1f82b24934e0e43bf2774a0e
AleksaMCode <aleksamcode@gmail.com>
2023-12-03 23:52:10 +0000

    Resolve `RuntimeError`

--- sniffer/parser.py
+++ sniffer/parser.py
@@ -15,15 +15,14 @@ def parse_traffic_file() -> Dict[str, str]:
     ssids = {}
     # airodump-ng names output with an automatic numbers, where the first file contains '-01'.
     filename = os.path.join(os.path.dirname(__file__), f"{TRAFFIC_FILE}-01.cap")
-    capture = pyshark.FileCapture(filename)
-
-    # TODO: Optimize by implementing skipping already read packages.
-    for package in capture:
-        # Filter only Probe Request and ignore Probe Requests with wildcard in the SSID field.
-        if int(package["wlan"].fc, 16) == 16384:
-            ssid = package["wlan.mgt"].wlan_tag.split(":")[2].strip().strip('"')
-            if "Wildcard" not in ssid:
-                ssids[ssid] = datetime.utcfromtimestamp(
-                    float(package.sniff_timestamp)
-                ).strftime("%Y-%m-%d %H:%M:%S")
+    with pyshark.FileCapture(filename) as capture:
+        # TODO: Optimize by implementing skipping already read packages.
+        for package in capture:
+            # Filter only Probe Request and ignore Probe Requests with wildcard in the SSID field.
+            if int(package["wlan"].fc, 16) == 16384:
+                ssid = package["wlan.mgt"].wlan_tag.split(":")[2].strip().strip('"')
+                if "Wildcard" not in ssid:
+                    ssids[ssid] = datetime.utcfromtimestamp(
+                        float(package.sniff_timestamp)
+                    ).strftime("%Y-%m-%d %H:%M:%S")
     return ssids

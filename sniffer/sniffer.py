981cbcd3f26b3f052ad4991d0e3f5523e4035245
AleksaMCode <aleksamcode@gmail.com>
2023-12-03 14:40:52 +0100

    Remove file path bug

--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -9,7 +9,7 @@ def clear_old_traffic():
     """
     Removes old traffic files
     """
-    for file in os.listdir("."):
+    for file in os.listdir(os.path.dirname(__file__)):
         if re.match(f"{TRAFFIC_FILE}.*\.cap", file):
             os.remove(file)
 
@@ -19,6 +19,6 @@ def capture_traffic(interface):
     Capture Wi-Fi traffic using airodump-ng and store data in a cap file.
     """
     subprocess.run(
-        ["airodump-ng", "-w", TRAFFIC_FILE, "--output-format", "cap", interface],
+        ["airodump-ng", "-w", os.path.join(os.path.dirname(__file__),TRAFFIC_FILE), "--output-format", "cap", interface],
         capture_output=True,
     )

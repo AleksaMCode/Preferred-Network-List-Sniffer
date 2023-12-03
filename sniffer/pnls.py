43534d1b847d426c695dd70c03b6d5372d810abd
AleksaMCode <aleksamcode@gmail.com>
2023-12-03 15:01:11 +0100

    Add new thread for parsing and sending of data

--- sniffer/pnls.py
+++ sniffer/pnls.py
@@ -1,9 +1,12 @@
 import sys
 import threading
 
+from db_client import send_data
 from sniffer import clear_old_traffic, capture_traffic
 
 clear_old_traffic()
 
 t1 = threading.Thread(target=capture_traffic(f"{sys.argv[1]}mon"), args=[])
+t2 = threading.Thread(target=send_data(), args=[])
 t1.start()
+t2.start()

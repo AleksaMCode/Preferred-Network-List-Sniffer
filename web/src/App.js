a438fb8d5630e68e82b91bf391a8e04c50db12f0
AleksaMCode <aleksamcode@gmail.com>
2023-12-04 17:05:03 +0100

    Import react-bootstrap css

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (

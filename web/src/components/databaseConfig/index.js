501329a078e55d25783c123eec844c6617192460
AleksaMCode <aleksamcode@gmail.com>
2023-12-04 16:39:26 +0100

    Init `firebase` store

---
+++ web/src/components/databaseConfig/index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+function InitFirebase() {
+  const firebaseConfig = {
+    apiKey: process.env.firebaseApiKey,
+    authDomain: process.env.firebaseAuthDomain,
+    databaseURL: process.env.firebaseDatabaseURL,
+    projectId: process.env.firebaseProjectId,
+    storageBucket: process.env.firebaseStorageBucket,
+    messagingSenderId: process.env.firebaseMessagingSenderId,
+    appId: process.env.firebaseAppId,
+  };
+  const app = initializeApp(firebaseConfig);
+  return getFirestore(app);
+}

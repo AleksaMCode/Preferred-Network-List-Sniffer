7f0999d3c5f62bd41c7f9199a24fae3988cef0d4
AleksaMCode <aleksamcode@gmail.com>
2023-12-06 20:54:21 +0100

    Add yaspin

--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -1,7 +1,9 @@
 from scapy.all import sniff
 from parser import parse_ip_packet
+from yaspin import yaspin
 
 
+@yaspin(text="Capturing SSIDs...")
 def capture_traffic(interface):
     """
     Capture Wi-Fi traffic and store captured SSIDs.

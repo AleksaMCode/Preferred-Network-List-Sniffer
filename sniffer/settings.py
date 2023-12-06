f04d8174727b5bf2b2fdf52103be2a548134c0c5
AleksaMCode <aleksamcode@gmail.com>
2023-12-06 13:45:57 +0100

    Add comments

--- sniffer/settings.py
+++ sniffer/settings.py
@@ -3,6 +3,11 @@ import os
 
 CONFIG_FILE = json.load(open(os.path.join(os.path.dirname(__file__), "sniffer.config")))
 
+# Format of the timestamp that will be stored alongside SSID.
 TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+# Name of the files that contain sniffed Wi-Fi traffic.
 TRAFFIC_FILE = "traffic"
+
+# Name of the file that contains Firebase credentials.
 FIREBASE_CREDENTIALS = "firebase_credentials.json"

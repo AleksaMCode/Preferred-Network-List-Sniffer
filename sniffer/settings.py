95fbdd327f09d760b6da6443b8ce0640b44bd613
AleksaMCode <aleksamcode@gmail.com>
2023-12-06 20:45:28 +0100

    Add firebase node name

--- sniffer/settings.py
+++ sniffer/settings.py
@@ -1,5 +1,6 @@
 import json
 import os
+import datetime
 
 CONFIG_FILE = json.load(open(os.path.join(os.path.dirname(__file__), "sniffer.config")))
 
@@ -11,6 +12,10 @@ TRAFFIC_FILE = "traffic"
 
 # Name of the file that contains Firebase credentials.
 FIREBASE_CREDENTIALS = "firebase_credentials.json"
+
+# Timestamp is used to name the main node for storing data.
+# Format is 'year + month + day', e.q. 20231202.
+FIREBASE_NODE = datetime.datetime.now().strftime("%Y%m%d")
 
 # Logging configuration.
 LOGGING = {

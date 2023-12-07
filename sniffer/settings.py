3421a20ce40bb55269fd1fb75b90c2390560d63b
AleksaMCode <aleksamcode@gmail.com>
2023-12-07 13:50:19 +0100

    Remove unused constant

--- sniffer/settings.py
+++ sniffer/settings.py
@@ -7,9 +7,6 @@ CONFIG_FILE = json.load(open(os.path.join(os.path.dirname(__file__), "sniffer.co
 # Format of the timestamp that will be stored alongside SSID.
 TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M:%S"
 
-# Name of the files that contain sniffed Wi-Fi traffic.
-TRAFFIC_FILE = "traffic"
-
 # Name of the file that contains Firebase credentials.
 FIREBASE_CREDENTIALS = "firebase_credentials.json"
 

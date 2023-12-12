80eb6481ef0dbaf0636b62b7e204ce523fccb1e0
AleksaMCode <aleksamcode@gmail.com>
2023-12-12 19:49:30 +0100

    Update parsing function Add wrapper function Remove Firebase Add push to channel function

--- sniffer/parser.py
+++ sniffer/parser.py
@@ -1,30 +1,24 @@
-import firebase_admin
-from firebase_admin import db, credentials
+import json
 
 from datetime import datetime
 from scapy.layers.dot11 import Dot11ProbeReq
 
-from settings import CONFIG_FILE, FIREBASE_CREDENTIALS, FIREBASE_NODE
+from settings import CHANNEL_ID
 from settings import TIMESTAMP_FORMAT
 
-firebase_admin.initialize_app(
-    credentials.Certificate(FIREBASE_CREDENTIALS),
-    {list(CONFIG_FILE.keys())[0]: CONFIG_FILE[list(CONFIG_FILE.keys())[0]]},
-)
 
-
-def parse_ip_packet(packet):
-    """
-    Filters the packet and uploads sniffed data (SSID + timestamp) to a Firebase database using `update` function.
-    """
-    # Filter only Probe Request and ignore Probe Requests with wildcard in the SSID field.
-    if packet.haslayer(Dot11ProbeReq):
-        ssid = packet.info.decode("utf-8")
-        if ssid:
-            db.reference(f"/{FIREBASE_NODE}").update(
-                {
+def parse_ip_packet_wrapper(socket_manager):
+    def parse_ip_packet(packet):
+        """
+        Filters the packet and broadcasts sniffed data (SSID + timestamp) to a Redis channel.
+        """
+        # Filter only Probe Request and ignore Probe Requests with wildcard in the SSID field.
+        if packet.haslayer(Dot11ProbeReq):
+            ssid = packet.info.decode("utf-8")
+            if ssid:
+                # await
+                socket_manager.broadcast_to_room(CHANNEL_ID, json.dumps({
                     ssid: datetime.utcfromtimestamp(float(packet.time)).strftime(
                         TIMESTAMP_FORMAT
                     )
-                }
-            )
+                }))

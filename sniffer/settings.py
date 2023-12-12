bb76831002c798862c75b6551f204845147ae902
AleksaMCode <aleksamcode@gmail.com>
2023-12-12 19:45:57 +0100

    Change `FIREBASE_NODE` to `CHANNEL_ID`

--- sniffer/settings.py
+++ sniffer/settings.py
@@ -10,9 +10,9 @@ TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M:%S"
 # Name of the file that contains Firebase credentials.
 FIREBASE_CREDENTIALS = "firebase_credentials.json"
 
-# Timestamp is used to name the main node for storing data.
+# Timestamp is used to name the channel id for storing data.
 # Format is 'year + month + day', e.q. 20231202.
-FIREBASE_NODE = datetime.datetime.now().strftime("%Y%m%d")
+CHANNEL_ID = datetime.datetime.now().strftime("%Y%m%d")
 
 # Logging configuration.
 LOGGING = {

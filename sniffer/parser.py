c19761b875565a39ed8d1026d11175c0acf3e991
AleksaMCode <aleksamcode@gmail.com>
2023-12-12 19:50:32 +0100

    Remove bug due to typo

--- sniffer/parser.py
+++ sniffer/parser.py
@@ -5,9 +5,10 @@ from scapy.layers.dot11 import Dot11ProbeReq
 
 from settings import CHANNEL_ID
 from settings import TIMESTAMP_FORMAT
+from websocket.websocket_manager import WebSocketManager
 
 
-def parse_ip_packet_wrapper(socket_manager):
+def parse_ip_packet_wrapper(socket_manager: WebSocketManager):
     def parse_ip_packet(packet):
         """
         Filters the packet and broadcasts sniffed data (SSID + timestamp) to a Redis channel.
@@ -17,7 +18,7 @@ def parse_ip_packet_wrapper(socket_manager):
             ssid = packet.info.decode("utf-8")
             if ssid:
                 # await
-                socket_manager.broadcast_to_room(CHANNEL_ID, json.dumps({
+                socket_manager.broadcast_to_channel(CHANNEL_ID, json.dumps({
                     ssid: datetime.utcfromtimestamp(float(packet.time)).strftime(
                         TIMESTAMP_FORMAT
                     )

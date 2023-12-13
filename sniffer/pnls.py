63c223d48343ee9015de3f6ce10a0e573c8c29e6
AleksaMCode <aleksamcode@gmail.com>
2023-12-13 18:48:17 +0100

    Update to be a backend starting point

--- sniffer/pnls.py
+++ sniffer/pnls.py
@@ -1,27 +1,61 @@
-import sys
-from http.client import HTTPException
-from urllib.error import HTTPError
+from pathlib import Path
 
-from settings import LOGGING
-from sniffer import capture_traffic
+import uvicorn
+from starlette import status
+
+from settings import LOGGING, SERVER, CHANNEL_ID
 from loguru import logger
 
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, WebSocketException
+
+from message_broker.websocket_broker import WebSocketBroker
+
+# Create logger.
 logger.add(
-    LOGGING["filename"],
+    f"{Path(__file__).stem}.log",
     format=LOGGING["format"],
     rotation=LOGGING["rotation"],
     retention=LOGGING["rotation"],
 )
 
+socket_manager = WebSocketBroker()
+app = FastAPI()
+
+
+@app.websocket("/ws/pub/{channel_id}")
+async def publisher(websocket: WebSocket, channel_id: str):
+    # Raise Exception if the channel id is incorrect.
+    if channel_id != CHANNEL_ID:
+        raise WebSocketException(code=status.WS_1014_BAD_GATEWAY)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            if data:
+                _ = await socket_manager.broadcast_to_channel(channel_id, data)
+    except WebSocketDisconnect:
+        logger.warning(f"Web client disconnected from the channel {channel_id}.")
+    except Exception as e:
+        logger.exception(f"Exception occurred: {str(e)}.")
+
+
+@app.websocket("/ws/sub/{channel_id}")
+async def subscriber(websocket: WebSocket, channel_id: str):
+    # Raise Exception if the channel id is incorrect.
+    if channel_id != CHANNEL_ID:
+        raise WebSocketException(code=status.WS_1014_BAD_GATEWAY)
+
+    await socket_manager.add_user_to_channel(channel_id, websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            if data:
+                websocket.send()
+    except WebSocketDisconnect:
+        logger.warning(f"Web client disconnected from the channel {channel_id}.")
+    except Exception as e:
+        logger.exception(f"Exception occurred: {str(e)}.")
+
+
 if __name__ == "__main__":
-    while True:
-        try:
-            logger.info("Capture packages from Wi-Fi traffic.")
-            capture_traffic(f"{sys.argv[1]}mon")
-        except (HTTPException, HTTPError) as e:
-            logger.exception(f"HTTP Exception: {str(e)}")
-        except KeyboardInterrupt as e:
-            logger.warning("Sniffer stopped forcefully.")
-            sys.exit(130)
-        except Exception as e:
-            logger.exception(str(e))
+    uvicorn.run(f"{Path(__file__).stem}:app", host=SERVER["host"], port=SERVER["port"], reload=True)

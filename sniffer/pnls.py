5f0b22df76eec84a4706fe6cf4c41e5dd45c5cae
AleksaMCode <aleksamcode@gmail.com>
2023-12-15 17:21:29 +0100

    :wheelchair::lock: Add `CORS` middleware

--- sniffer/pnls.py
+++ sniffer/pnls.py
@@ -2,14 +2,24 @@ from pathlib import Path
 
 import uvicorn
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from logger import create_logger
 from services import pub_sub
 from settings import SERVER
 
+origins = ["*"]
+
 create_logger(f"{Path(__file__).stem}.log")
 app = FastAPI()
+
 app.include_router(pub_sub.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(

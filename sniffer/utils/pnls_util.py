26b98defb2643579410a3d5a859c7cd170094f21
AleksaMCode <aleksamcode@gmail.com>
2023-12-21 15:14:45 +0100

    Update error handle

--- sniffer/utils/pnls_util.py
+++ sniffer/utils/pnls_util.py
@@ -2,10 +2,11 @@ import os
 import signal
 from contextlib import asynccontextmanager
 
+import redis
 from fastapi import FastAPI
 
-from logger import log_info_async, log_error
-from message_broker.websocket_broker import create_broker, socket_manager
+from logger import log_info_async, log_error, log_exception_async
+from message_broker.websocket_broker import create_broker, socket_broker
 
 
 @asynccontextmanager
@@ -13,12 +14,16 @@ async def lifespan(app: FastAPI):
     await log_info_async("Server starting.")
     try:
         await create_broker()
-    except Exception:
+    except redis.exceptions.ConnectionError as e:
         # Shut down server if the PNLS fails to create a broker.
+        await log_exception_async(f"Failed to establish connection with Redis server due to an Exception: {str(e)}")
+    except Exception as e:
+        await log_exception_async(f"An exception occurred during the server start up : {str(e)}")
+    finally:
         shutdown()
     yield
     await log_info_async("Server shutting down.")
-    await socket_manager.close_sockets()
+    await socket_broker.close_sockets()
 
 
 def shutdown():
